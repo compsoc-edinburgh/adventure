c3c9f07f13e24feef3b91e0dc67127960e1bd057
Yuto Takano <moa17stock@gmail.com>
2024-12-01 16:10:04 +0100

    Show delete buttons and tips on hover for transactions panel

--- src/components/MyTransactions.tsx
+++ src/components/MyTransactions.tsx
@@ -3,6 +3,7 @@ import { User, ShopItem, ShopTransaction } from "../sqlite.server";
 import { useFetcher } from "@remix-run/react";
 import { action as shopCancelAction } from "../routes/shop.cancel";
 import Stars from "./Stars";
+import { IoMdCloseCircle } from "react-icons/io";
 
 type MyTransactionsProps = {
   user?: User;
@@ -14,13 +15,17 @@ type MyTransactionsProps = {
 export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ user, remaining_stars, shop_items, transactions }) => {
   const fetcher = useFetcher<typeof shopCancelAction>();
   return (
-    <div className="md:self-start self-stretch mx-4 shadow-christmasBeigeAccent">
-      <div className="bg-white shadow-sm min-w-64 max-w-md rounded-lg">
+    <div className="md:self-start self-stretch mx-4 shadow-christmasBeigeAccent group">
+      <div className="bg-white shadow-sm min-w-96 md:max-w-md rounded-lg">
         <div className="p-6">
-          <h1 className="text-3xl font-display mb-4">Santa, I would like...</h1>
+          <span className="text-sm opacity-50">
+            Purchases
+            {user && ` for AoC#${user.aoc_id}`}
+          </span>
+          <h1 className="text-3xl font-display mb-4">Dear Santa, I want...</h1>
           {fetcher.data && !fetcher.data["success"] && <p>{fetcher.data["message"]}</p>}
           {transactions.filter(t => !t.cancelled_at).length === 0 && (
-            <p className="opacity-50">Nothing. That's fine too.</p>
+            <p className="opacity-50">...Nothing! That's fine too.</p>
           )}
           <ul>
             {transactions.map(transaction => (
@@ -32,12 +37,14 @@ export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ user, r
                         {shop_items.find(i => i.id === transaction.shop_item_id)?.name}
                       </span>
                       <Stars className="z-10 bg-white pl-2" amount={shop_items.find(i => i.id === transaction.shop_item_id)?.star_cost || 0} />
-                    </div>
 
-                    <fetcher.Form method="post" action="shop/cancel">
-                      <input type="hidden" name="transaction_id" value={transaction.id} />
-                      <button type="submit">X</button>
-                    </fetcher.Form>
+                      <fetcher.Form method="post" action="shop/cancel" className="opacity-0 group-hover:opacity-100 transition-all duration-75 absolute top-1/2 -translate-y-1/2 -right-4 group-hover:-right-5 flex items-center text-lg">
+                        <input type="hidden" name="transaction_id" value={transaction.id} />
+                        <button type="submit" title="Remove this item">
+                          <IoMdCloseCircle />
+                        </button>
+                      </fetcher.Form>
+                    </div>
                   </li>
                 )}
               </React.Fragment>
@@ -46,7 +53,7 @@ export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ user, r
         </div>
       </div>
 
-      <div className="bg-white shadow-sm rounded-lg group min-w-64 border-t-2 border-christmasGreenAccent">
+      <div className="bg-white shadow-sm rounded-lg group min-w-64 md:max-w-md border-t-2 border-christmasGreenAccent">
         <div className="px-6 pb-3 pt-3">
           {user && (
             <div className="flex flex-row justify-between">
@@ -78,6 +85,16 @@ export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ user, r
           )}
         </div>
       </div>
+
+      {user
+      && (
+        <div className="bg-white transition-all opacity-0 group-hover:opacity-75 -mt-1 group-hover:mt-0 shadow-sm rounded-lg group min-w-64 md:max-w-md border-t-2 border-christmasGreenAccent">
+          <div className="px-6 pb-3 pt-3">
+            <p><b>That's it!</b></p>
+            <p className="text-sm">Once you've chosen your rewards and they are listed above, you're good to go. Once January comes around, you'll be able to collect these physically.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
