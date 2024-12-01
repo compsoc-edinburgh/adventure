7782bba5819d06f09de4c273df199d733b4555c5
Yuto Takano <moa17stock@gmail.com>
2024-12-01 14:54:13 +0100

    Fix AoC star API erroring when user ID not found

--- src/stars.server.tsx
+++ src/stars.server.tsx
@@ -5,5 +5,5 @@ const AOC_DATA_FILE = "data/aoc_data.json";
 
 export function getStarsForUser(aoc_user_id: number): number {
   const data = JSON.parse(fs.readFileSync(AOC_DATA_FILE, "utf-8"));
-  return data.members[aoc_user_id.toString()].stars;
+  return data.members[aoc_user_id.toString()]?.stars || 0;
 }
