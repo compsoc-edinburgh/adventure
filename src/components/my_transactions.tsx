653d83989200c8c8b54f38ae8e12b71270b61b3d
Yuto Takano <moa17stock@gmail.com>
2024-12-01 13:44:18 +0100

    Style My Transactions panel

--- src/components/my_transactions.tsx
+++ src/components/my_transactions.tsx
@@ -1,37 +1,58 @@
-import { FunctionComponent } from "react";
-import { ShopTransaction } from "../sqlite.server";
+import React, { FunctionComponent } from "react";
+import { ShopItem, ShopTransaction } from "../sqlite.server";
 import { useFetcher } from "@remix-run/react";
 import { action as shopCancelAction } from "../routes/shop.cancel";
 
 type MyTransactionsProps = {
+  remaining_stars: number;
+  shop_items: ShopItem[];
   transactions: ShopTransaction[];
 };
 
-export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ transactions }) => {
+export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ remaining_stars, shop_items, transactions }) => {
   const fetcher = useFetcher<typeof shopCancelAction>();
   return (
-    <div>
-      <h1>My Transactions</h1>
-      {fetcher.data && !fetcher.data["success"] && <p>{fetcher.data["message"]}</p>}
-      <ul>
-        {transactions.map(transaction => (
-          <li key={transaction.id}>
-            {transaction.transaction_time}
-            {" "}
-            {transaction.shop_item_id}
-            {" "}
-            -
-            {" "}
-            {transaction.user_id}
+    <div className="bg-white mx-4 rounded-md group min-w-64">
+      <div className="px-6 pt-6 pb-3">
+        <h1 className="text-3xl font-display mb-4">I want...</h1>
+        {fetcher.data && !fetcher.data["success"] && <p>{fetcher.data["message"]}</p>}
+        <ul>
+          {transactions.map(transaction => (
+            <React.Fragment key={transaction.id}>
+              {!transaction.cancelled_at && (
+                <li>
+                  <span className="block font-semibold">
+                    {shop_items.find(i => i.id === transaction.shop_item_id)?.name}
+                  </span>
+                  <span className="flex flex-row justifybetween">
+                    <span>
+                      {transaction.transaction_time}
+                    </span>
+                    <span>
+                      ⭐
+                      {" "}
+                      {shop_items.find(i => i.id === transaction.shop_item_id)?.star_cost}
+                    </span>
+                  </span>
 
-            {!transaction.cancelled_at ? "Valid" : "Invalid"}
-            <fetcher.Form method="post" action="shop/cancel">
-              <input type="hidden" name="transaction_id" value={transaction.id} />
-              <button type="submit">X</button>
-            </fetcher.Form>
-          </li>
-        ))}
-      </ul>
+                  <fetcher.Form method="post" action="shop/cancel">
+                    <input type="hidden" name="transaction_id" value={transaction.id} />
+                    <button type="submit">X</button>
+                  </fetcher.Form>
+                </li>
+              )}
+            </React.Fragment>
+          ))}
+        </ul>
+      </div>
+      <div className="w-full border-t-4 border-dotted border-christmasGreen" />
+      <div className="px-6 pb-6 pt-3 flex flex-row justify-between">
+        <span className="capitalize">Remaining</span>
+        <span>
+          ⭐
+          {remaining_stars}
+        </span>
+      </div>
     </div>
   );
 };
