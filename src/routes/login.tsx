821869f8204fe339897b8c0739cdfde54e96e8fd
Yuto Takano <moa17stock@gmail.com>
2024-12-01 16:50:41 +0100

    Style login page

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -6,11 +6,17 @@ import { getSession, commitSession } from "../sessions";
 export default function LoginForm() {
   const { error } = useLoaderData<typeof loader>();
   return (
-    <div>
+    <div className="flex items-center justify-center w-full flex-col">
+      <h2 className="text-3xl font-display mb-6">Login</h2>
+      <span className="text-sm opacity-75">Please enter your AoC ID to access the reward exchange shop.</span>
       {error && <p>{error}</p>}
-      <Form method="post">
-        <input type="text" name="aoc_id" placeholder="AoC ID" className="text-black" />
-        <button type="submit">Access Shop</button>
+      <Form method="post" className="mt-6">
+
+        <div className="min-w-64">
+          <label htmlFor="aoc_id" className="block mb-2 text-sm text-christmasDark">AoC ID Number</label>
+          <input type="text" name="aoc_id" placeholder="e.g. 1234512" id="aoc_id" className="bg-gray-50 border border-christmasBeigeAccent  text-sm rounded-lg focus:outline-double focus:outline-4 focus:outline-christmasRed box-border block w-72 p-3" required />
+        </div>
+        <button type="submit" className="block relative w-full mt-3 px-6 py-2 rounded-lg bg-christmasRed text-white active:scale-95 transition-all duration-75 focus:outline-4 focus:outline-christmasRed focus:outline-double">Access Shop</button>
       </Form>
     </div>
   );
