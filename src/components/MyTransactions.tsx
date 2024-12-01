0e91ffecc00201dcb162b411dc38a06190ff0402
Yuto Takano <moa17stock@gmail.com>
2024-12-01 22:58:39 +0100

    Fix Transaction tips being invisible on mobile screens

--- src/components/MyTransactions.tsx
+++ src/components/MyTransactions.tsx
@@ -92,7 +92,7 @@ export const MyTransactions: FunctionComponent<MyTransactionsProps> = ({ user, r
 
       {user
       && (
-        <div className="bg-white transition-all opacity-0 group-hover:opacity-75 -mt-1 group-hover:mt-0 shadow-sm rounded-lg group min-w-64 md:max-w-md border-t-2 border-christmasGreenAccent">
+        <div className="bg-white transition-all opacity-75 lg:opacity-0 group-hover:opacity-75 -mt-1 group-hover:mt-0 shadow-sm rounded-lg group min-w-64 lg:max-w-md border-t-2 border-christmasGreenAccent">
           <div className="px-6 pt-3">
             {transactions.filter(t => !t.cancelled_at).length > 0 && (
               <div className="pb-3">
