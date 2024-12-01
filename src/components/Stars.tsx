d74edf944d7bfc5e31cc9f4e6753817f6a67c6a9
Yuto Takano <moa17stock@gmail.com>
2024-12-01 22:15:25 +0100

    Fix Stars component having high-priority red text colour

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,7 +1,7 @@
 import { FaStar } from "react-icons/fa";
 
 const Stars = ({ amount, className }: { amount: number; className?: string }) => (
-  <span className={`flex items-center text-lg font-display gap-1 text-christmasRedAccent ${className}`}>
+  <span className={`flex items-center text-lg font-display gap-1 ${className}`}>
     {amount}
     <FaStar />
   </span>
