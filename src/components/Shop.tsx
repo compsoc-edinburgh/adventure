2059dec360d21cd6a29a385700fc6da465738cef
Yuto Takano <moa17stock@gmail.com>
2024-12-01 19:29:26 +0100

    Add shadow to 'Reward Shop' heading, make shop items all dark BG-ed

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -13,30 +13,30 @@ export const Shop: FunctionComponent<ShopProps> = ({ shop_items }) => {
   return (
     <div>
       <h2 className="text-3xl font-display mb-4 relative justify-self-start">
-        <div className="w-full bg-christmasBeige h-full absolute top-0 left-0 z-[-1] scale-110 -skew-x-12 -rotate-2 transform-gpu" />
+        <div className="w-full bg-christmasBeige h-full absolute top-0 left-0 z-[-1] scale-110 -skew-x-12 -rotate-2 transform-gpu shadow-md" />
         Reward Shop
       </h2>
       {fetcher.data && !fetcher.data["success"] && <p>{fetcher.data["message"]}</p>}
       <ul className="grid grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 gap-4">
         {shop_items.map((item, i) => (
-          <li key={item.id} className={"bg-christmasBeigeAccent transform-gpu overflow-hidden rounded-xl group relative flex flex-col " + (i == 0 ? "row-span-2 col-span-2" : "row-span-1 col-span-1")}>
+          <li key={item.id} className={(i % 3 == 0 ? "bg-christmasGreen" : i % 3 == 1 ? "bg-christmasRed" : "bg-christmasDark") + " transform-gpu overflow-hidden rounded-xl group relative flex flex-col " + (i == 0 ? "row-span-2 col-span-2" : "row-span-1 col-span-1")}>
             <img src={item.image_url} alt={item.name} className="[mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] group-hover:scale-105 transition-all duration-150" />
             <div className="absolute bottom-0 flex transform-gpu flex-col gap-1 p-6 transition-all duration-150 group-hover:-translate-y-10 w-full">
-              <h3 className="text-xl font-semibold text-christmasDark dark:text-neutral-300">
+              <h3 className="text-xl font-semibold text-white">
                 {item.name}
               </h3>
-              <p className="max-w-lg text-christmasDark opacity-50">{item.description}</p>
+              <p className="max-w-lg text-white">{item.description}</p>
             </div>
             <div
               className="absolute bottom-0 w-full translate-y-10 transform-gpu flex justify-between items-center p-6 opacity-0 transition-all duration-150 group-hover:translate-y-0 group-hover:opacity-100"
             >
-              <Stars amount={item.star_cost} />
+              <Stars amount={item.star_cost} className="text-christmasBeige" />
               <fetcher.Form method="post" action="shop/purchase">
                 <input type="hidden" name="shop_item_id" value={item.id} />
-                <button type="submit" className="pointer-events-auto bg-christmasRed text-white rounded-md py-2 px-4 active:scale-95 transition-all duration-75 focus:outline-4 focus:outline-christmasRed focus:outline-double">Exchange</button>
+                <button type="submit" className="pointer-events-auto bg-christmasBeigeAccent text-christmasDark rounded-md py-2 px-4 active:scale-95 transition-all duration-75 focus:outline-4 focus:outline-christmasBeigeAccent focus:outline-double">Exchange</button>
               </fetcher.Form>
             </div>
-            <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-150 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
+            <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-150 group-hover:bg-black/[.03]" />
             {" "}
           </li>
         ))}
