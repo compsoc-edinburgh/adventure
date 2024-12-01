9666faf02aa5c191b650ff1107bcbe0cc11bb2ea
Yuto Takano <moa17stock@gmail.com>
2024-12-01 16:35:06 +0100

    Move decorations, heading and login into root layout to apply to all pages

--- src/root.tsx
+++ src/root.tsx
@@ -3,10 +3,18 @@ import {
   Meta,
   Outlet,
   Scripts,
+  useLoaderData,
 } from "@remix-run/react";
 
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import stylesheet from "./tailwind.css?url";
+import SnowParticles from "./components/Particles";
+import { CompSocLogo } from "./components/CompSocLogo";
+import { Ornament1 } from "./components/Ornament1";
+import { Tree } from "./components/Tree";
+import UserLogin from "./components/UserLogin";
+import { getSession } from "./sessions";
+import { getUserById, User } from "./sqlite.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -18,7 +26,27 @@ export const links: LinksFunction = () => [
   },
 ];
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(
+    request.headers.get("Cookie"),
+  );
+
+  const response: { user: User | undefined } = {
+    user: undefined,
+  };
+
+  if (session.has("user_id")) {
+    const user_id = session.get("user_id");
+    if (user_id) {
+      response.user = getUserById(parseInt(user_id));
+    };
+  }
+
+  return response;
+};
+
 export default function App() {
+  const { user } = useLoaderData<typeof loader>();
   return (
     <html lang="en-us">
       <head>
@@ -30,7 +58,30 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-christmasBeige text-christmasDark">
-        <Outlet />
+
+        <div className="w-full">
+          <div className="absolute inset-0 top-0 left-0 overflow-hidden -z-10">
+            <Tree className="absolute h-full -left-12" />
+          </div>
+          <div className="flex flex-col mt-8 mb-8 items-center">
+            <CompSocLogo className="w-32 opacity-50 mb-4" />
+            <h1 className="md:text-7xl text-4xl text-center font-display bg-clip-text text-transparent bg-gradient-to-t from-christmasDark to-christmasRed">Advent of Code Rewards</h1>
+            <Ornament1 className="w-56 -mt-3" />
+          </div>
+          <UserLogin user={user} className="absolute top-0 right-0" />
+
+          <Outlet />
+        </div>
+
+        <SnowParticles
+          className="absolute inset-0"
+          quantity={100}
+          ease={80}
+          color="#ffffff"
+          size={3}
+          staticity={60}
+          vy={1.1}
+        />
         <Scripts />
       </body>
     </html>
--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,15 +1,10 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
-import UserLogin from "../components/UserLogin";
 import { getSession } from "../sessions";
 import { useLoaderData } from "@remix-run/react";
 import { Shop } from "../components/Shop";
 import { MyTransactions } from "../components/MyTransactions";
 import { getShopItems, getTransactionsByUserId, getUserById, ShopItem, ShopTransaction, updateUserStars, User } from "../sqlite.server";
-import SnowParticles from "../components/Particles";
 import { getStarsForUser } from "../stars.server";
-import { Tree } from "../components/Tree";
-import { Ornament1 } from "../components/Ornament1";
-import { CompSocLogo } from "../components/CompSocLogo";
 
 export async function loader({
   request,
@@ -56,34 +51,11 @@ export default function Index() {
   const { user, remaining_stars, shop_items, transactions } = useLoaderData<typeof loader>();
 
   return (
-    <div className="w-full">
-      <div className="absolute inset-0 top-0 left-0 overflow-hidden -z-10">
-        <Tree className="absolute h-full -left-12" />
-      </div>
-      <div className="flex flex-col mt-8 mb-8 items-center">
-        <CompSocLogo className="w-32 opacity-50 mb-4" />
-        <h1 className="md:text-7xl text-4xl text-center font-display bg-clip-text text-transparent bg-gradient-to-t from-christmasDark to-christmasRed">Advent of Code Rewards</h1>
-        <Ornament1 className="w-56 -mt-3" />
-      </div>
-
-      <div className="flex md:flex-row flex-col">
-        <div className="md:block hidden flex-grow" />
-        <div className="flex flex-col">
-          <UserLogin user={user} className="absolute top-0 right-0" />
-          <Shop shop_items={shop_items} />
-        </div>
-        <MyTransactions user={user} remaining_stars={remaining_stars} shop_items={shop_items} transactions={transactions} />
-        <div className="md:block hidden w-16" />
-      </div>
-      <SnowParticles
-        className="absolute inset-0"
-        quantity={100}
-        ease={80}
-        color="#ffffff"
-        size={3}
-        staticity={60}
-        vy={1.1}
-      />
+    <div className="flex md:flex-row flex-col">
+      <div className="md:block hidden flex-grow" />
+      <Shop shop_items={shop_items} />
+      <MyTransactions user={user} remaining_stars={remaining_stars} shop_items={shop_items} transactions={transactions} />
+      <div className="md:block hidden w-16" />
     </div>
   );
 }
