b8acc268db68433ef8e3570afea59546ed8c3fea
Yuto Takano <moa17stock@gmail.com>
2024-12-02 16:16:36 +0100

    Make the star count in Stars component larger for legibility

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -2,7 +2,7 @@ import { FaStar } from "react-icons/fa";
 
 const Stars = ({ amount, className }: { amount: number; className?: string }) => (
   <span className={`flex items-center text-lg font-display gap-1 ${className}`}>
-    {amount}
+    <span className="text-xl">{amount}</span>
     <FaStar />
   </span>
 );
