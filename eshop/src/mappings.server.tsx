3b516f1896fcf0df984002ebe503b3a1add1b4a6
Yuto Takano <moa17stock@gmail.com>
2025-11-15 19:05:56 +0100

    eshop: Fix discord_mapping being searched without using ABS_DATA_DIR

--- eshop/src/mappings.server.tsx
+++ eshop/src/mappings.server.tsx
@@ -1,11 +1,13 @@
 import * as fs from "fs";
+import path from "path";
+import { env } from "process";
 
 // Files containing the API response from AoC. These are split into several
 // since each leaderboard can only contain 200 members. There may be duplicate
 // members in each file, but they should contain the same info if so.
-const MAPPING_FILE = "data/discord_mapping.json";
+const mapping_file = path.join(env.ABS_DATA_DIR as string, "discord_mapping.json");
 
 export function getDiscordIdFromAocId(aoc_user_id: number): number | null {
-  const data = JSON.parse(fs.readFileSync(MAPPING_FILE, "utf-8"));
+  const data = JSON.parse(fs.readFileSync(mapping_file, "utf-8"));
   return data[aoc_user_id.toString()] || null;
 }
