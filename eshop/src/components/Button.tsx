5be40134e11342624b88124e87c3a255129d9cbc
Yuto Takano <moa17stock@gmail.com>
2025-11-16 00:31:25 +0100

    eshop: Make disabled buttons appear disabled (esp. in admin view)

--- eshop/src/components/Button.tsx
+++ eshop/src/components/Button.tsx
@@ -10,7 +10,11 @@ type ButtonProps<C extends React.ElementType> = {
 export const Button = <C extends React.ElementType>({ className, bg = "red", component, children, ...props }: ButtonProps<C>) => {
   const Element = component || "button";
 
-  const bgClasses = bg === "red" ? "bg-christmasRed text-white focus:outline-christmasRed" : bg === "beige" ? "bg-christmasBeigeAccent text-christmasDark focus:outline-christmasBeigeAccent" : bg === "green" ? "bg-christmasGreen text-white focus:outline-christmasGreen" : "";
+  let bgClasses = bg === "red" ? "bg-christmasRed text-white focus:outline-christmasRed" : bg === "beige" ? "bg-christmasBeigeAccent text-christmasDark focus:outline-christmasBeigeAccent" : bg === "green" ? "bg-christmasGreen text-white focus:outline-christmasGreen" : "";
+
+  if (props.disabled === true) {
+    bgClasses = "bg-christmasBeige text-christmasDark text-opacity-50";
+  }
 
   return (
     <Element className={`block relative rounded-lg active:scale-95 transition-transform duration-75 focus:outline focus:outline-2 focus:outline-offset-2 ${bgClasses} ${className}`} {...props}>
