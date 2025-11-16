b4cf2be89fda6a1a9d3dc0ef3d9f9db5b3b9c14c
Yuto Takano <moa17stock@gmail.com>
2025-11-16 14:21:25 +0100

    eshop: Add subtitle under logo saying '2025 Edition', change colours from last year

--- eshop/src/root.tsx
+++ eshop/src/root.tsx
@@ -90,7 +90,8 @@ export default function App() {
               <CompSocLogo className="w-8 absolute left-0 top-0 m-4 opacity-50 hover:opacity-100" />
             </a>
             <Link to="/">
-              <h1 className="md:text-7xl text-4xl text-center font-display bg-clip-text text-transparent bg-gradient-to-t from-christmasDark to-christmasRed">Advent of Code Rewards</h1>
+              <h1 className="md:text-7xl text-4xl text-center font-display bg-clip-text text-transparent bg-gradient-to-t from-christmasRed to-christmasRedAccent">Advent of Code Rewards</h1>
+              <span className="text-right font-display block -mt-2">2025 Edition</span>
             </Link>
             <Ornament1 className="w-56 -mt-3" />
           </div>
