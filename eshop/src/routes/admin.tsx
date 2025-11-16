4be2cf9575478477f2b00598cc2bc65aec0fd852
Yuto Takano <moa17stock@gmail.com>
2025-11-16 11:57:10 +0100

    eshop: Show small help text when visiting /admin without selecting a tab

--- eshop/src/routes/admin.tsx
+++ eshop/src/routes/admin.tsx
@@ -1,10 +1,11 @@
-import { Link, Outlet, redirect, useNavigation, LoaderFunctionArgs } from "react-router";
+import { Link, redirect, useNavigation, LoaderFunctionArgs, useOutlet } from "react-router";
 import { requireUserSession } from "../sessions";
 import { getUserById } from "../sqlite.server";
 import { Button } from "../components/Button";
 
 export default function Admin() {
   const navigation = useNavigation();
+  const outlet = useOutlet();
   return (
     <div className="flex flex-col items-center md:max-w-[70%] m-auto">
       <h1 className="text-3xl font-display mb-3">Admin Page</h1>
@@ -13,11 +14,12 @@ export default function Admin() {
         <Button component={Link} to="/admin/items" bg="green" className="px-6 py-2">Items</Button>
         <Button component={Link} to="/admin/transactions" bg="green" className="px-6 py-2">Transactions</Button>
       </div>
+      {outlet === null && "Click on one of the categories to see a list of database rows for it."}
       {navigation.state === "loading"
         ? (
             <p>Loading...</p>
           )
-        : <Outlet />}
+        : outlet}
     </div>
   );
 }
